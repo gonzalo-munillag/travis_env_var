32d25bc5e7cbae4d166d5c457cfe24570111433a
Gonzalo Munilla Garrido <gonzalo.munilla-garrido@tum.de>
2021-07-03 12:12:58 +0200

    second commit

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const docker_username_value = process.env.DOCKER_USERNAME;
+
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -5,7 +7,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`My docker user name is ${docker_username_value}`);
     await sleep(5000);
   }
 }
